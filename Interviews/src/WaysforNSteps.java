0bfd5cd2fa921e10e07cf33cc8cb68a3f6ddd5bc
Sree Harsha Konduri <sreehars@buffalo.edu>
2014-01-01 18:40:19 -0500

    add dynamic programming solution

--- Interviews/src/WaysforNSteps.java
+++ Interviews/src/WaysforNSteps.java
@@ -17,7 +17,7 @@ private static void findWays(int n,int []options,int opt,ArrayList<Integer> res)
 		return;
 	}
 	
-	for(int i=opt;i < options.length;i++) {
+	for(int i=0;i < options.length;i++) {
 		result = n - options[i];
 		if(result >= 0) {
 		res.add(options[i]);
@@ -28,5 +28,23 @@ private static void findWays(int n,int []options,int opt,ArrayList<Integer> res)
 		findWays(result, options, i+1, res);
 	}
 	}
+}
+/**
+ *
+ * param n input
+ * this is a dynamic programming solution to solve this exponential problem
+ * runs in O(n) time.
+ */
+private static void dynamicSolution(int n) {
+	
+	java.math.BigInteger a = new java.math.BigInteger("1");
+    	java.math.BigInteger b = new java.math.BigInteger("2");
+	java.math.BigInteger r = new java.math.BigInteger("0");
+    	for(int i = 2;i < n ; i++) {
+	r = a.add(b);
+	a = b;
+	b = r;   	
+	}
+	System.out.println(r);
 }
 }
